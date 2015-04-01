a2bf4f9275f9b57dc96dd7deb0d388729f1b86ab
apewiki <zhu1996@yahoo.com>
2015-04-01 17:26:19 -0400

    Added new functionality to toggle priority. Modified index.html, todo-view.js, todo.js

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -21,6 +21,7 @@ var app = app || {};
 			'dblclick label': 'edit',
 			'click .edit-btn': 'edit',
 			'click .destroy': 'clear',
+			'click .priority-btn': 'setPriority',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
 			'blur .edit': 'close'
@@ -51,6 +52,7 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -75,6 +77,11 @@ var app = app || {};
 		edit: function () {
 			this.$el.addClass('editing');
 			this.$input.focus();
+		},
+
+		setPriority: function() {
+			//this.$el.toggleClass('priority');
+			this.model.togglePriority();
 		},
 
 		// Close the `"editing"` mode, saving changes to the todo.
